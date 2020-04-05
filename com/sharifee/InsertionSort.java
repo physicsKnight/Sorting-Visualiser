a23f7a03bb500027e6d6f43317f164998f8eaf7f
sharifee <umarsharif812@gmail.com>
2020-04-05 16:30:18 +0100

    implemented selection sort algorithm, fixed naming issues

--- com/sharifee/InsertionSort.java
+++ com/sharifee/InsertionSort.java
@@ -1,9 +1,10 @@
 package com.sharifee;
 
 public class InsertionSort implements SortAlgorithm {
-    // takes values and compares them with every value before it in the array
-    // and inserts them into the sorted section. The array is only passed
-    // through once by the outer loop.
+    // takes values in the unsorted portion and swaps them with every
+    // index before it until the value we are comparing is larger.
+    // we stop comparing (break out of loop) and move to the next
+    // unsorted value.
 
     @Override
     public void sort(int[] array) {
--- com/sharifee/SelectionSort.java
+++ com/sharifee/SelectionSort.java
@@ -1,9 +1,28 @@
 package com.sharifee;
 
 public class SelectionSort implements SortAlgorithm {
+    // searches for the smallest value in the unsorted portion of the array
+    // then swaps it into the next index of the array until
+    // it swaps the last unsorted values
 
     @Override
     public void sort(int[] array) {
-
+        for (int indexA = 0; indexA < array.length - 1; indexA++) {
+            // keep track of min value and position
+            int minVal = array[indexA];
+            int minIndex = indexA;
+            // search for min value
+            for (int indexB = indexA + 1; indexB < array.length; indexB++) {
+                int currVal = array[indexB];
+                if (currVal < minVal) {
+                    // update min value and position
+                    minVal = currVal;
+                    minIndex = indexB;
+                }
+            }
+            // swap values
+            array[minIndex] = array[indexA];
+            array[indexA] = minVal;
+        }
     }
 }
