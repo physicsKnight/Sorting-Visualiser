4d0657397693d8a195eba1aec94d8de008d0260a
sharifee <umarsharif812@gmail.com>
2020-04-06 17:03:30 +0100

    implemented quick sort algorithm

--- com/sharifee/QuickSort.java
+++ com/sharifee/QuickSort.java
@@ -4,6 +4,23 @@ public class QuickSort implements SortAlgorithm {
 
     @Override
     public void sort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
 
+    private void quickSort(int[] array, int left, int right) {
+        if (left < right) {
+            int pivot = array[right];
+            int insertIndex = left;
+            for (int l = left; l <= right; l++) {
+                int temp = array[i];
+                if (temp <= pivot) {
+                    array[i] = array[insertIndex];
+                    array[insertIndex] = temp;
+                    insertIndex++;
+                }
+            }
+            quickSort(array, left, insertIndex - 2);
+            quickSort(array, insertIndex, right);
+        }
     }
 }
